863407f478874126d78b5dc8f175fb850a5fc8ac
vimukthiwijeratne <vimukthi.wijeratne@gmail.com>
2023-03-14 20:15:20 +0530

    consolidated home page and made info page generic

--- lib/infoPage.dart
+++ lib/infoPage.dart
@@ -2,19 +2,19 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class infoPage extends StatelessWidget {
-  List topRated = [];
+  List movieList = [];
   int index;
   String color = "red";
   int size = 10;
 
-  infoPage({required this.topRated, required this.index});
+  infoPage({required this.movieList, required this.index});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.black,
         appBar: AppBar(
-          title: Text(topRated[index]['title']),
+          title: Text(movieList[index]['title']),
         ),
         body: Container(
             child: ListView(children: [
@@ -26,7 +26,7 @@ class infoPage extends StatelessWidget {
                       width: MediaQuery.of(context).size.width,
                       child: Image.network(
                           'http://image.tmdb.org/t/p/w500' +
-                              topRated[index]['backdrop_path'],
+                              movieList[index]['backdrop_path'],
                           fit: BoxFit.cover)))),
           //this is the row with poster and description
           Row(children: [
@@ -35,17 +35,17 @@ class infoPage extends StatelessWidget {
                 height: 200,
                 width: 100,
                 child: Image.network('http://image.tmdb.org/t/p/w500' +
-                    topRated[index]['poster_path'])),
+                    movieList[index]['poster_path'])),
             Flexible(
                 child: Container(
-                    child: Text(topRated[index]['overview'],
+                    child: Text(movieList[index]['overview'],
                         style: TextStyle(fontSize: 16, color: Colors.white))))
           ]),
           //this is the title under the poster
           Container(
               child: Text(
-                  topRated[index]['title'] != null
-                      ? topRated[index]['title']
+                  movieList[index]['title'] != null
+                      ? movieList[index]['title']
                       : 'Not Loaded',
                   style: TextStyle(
                       fontSize: 30,
@@ -55,14 +55,14 @@ class infoPage extends StatelessWidget {
           Row(children: [
             Container(
                 child: Row(children: [
-              Text('Rating - ' + topRated[index]['vote_average'].toString(),
+              Text('Rating - ' + movieList[index]['vote_average'].toString(),
                   style: TextStyle(color: Colors.white)),
               Icon(Icons.star, color: Colors.yellow, size: 20.0)
             ])),
             Container(
                 padding: EdgeInsets.only(left: 10),
                 child: Row(children: [
-                  Text('Release Date - ' + topRated[index]['release_date'],
+                  Text('Release Date - ' + movieList[index]['release_date'],
                       style: TextStyle(color: Colors.white)),
                   Icon(Icons.date_range, color: Colors.white, size: 20.1)
                 ]))
